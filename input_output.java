b2f6ad8b5b3327a869955a40a98d26b923557aa7
KIRTPREET KAUR <84916234+kirtpreetkaur@users.noreply.github.com>
2022-10-08 11:11:18 +0530

    Create input_output.java

---
+++ input_output.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        double b=sc.nextDouble();
+        sc.nextLine();
+        String str=sc.nextLine();
+        System.out.println("String: "+str);
+        System.out.println("Double: "+b);
+        System.out.println("Int: "+a); 
+    }
+}
